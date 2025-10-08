2320fab71d48b01427da39ff67b7d20e19cf3bba
kevin marcos <kevin.marcos.matos@escola.pr.gov.br>
2025-10-08 01:07:41 +0000

    aula de hoje

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Quimica',
+    'como ocorre uma ligaçã quimica?',
+    ' quando átomos se unem para atingir maior estabilidade, geralmente completando a camada de valência (regra do octeto) ao doar, receber ou compartilhar elétrons'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Portuguê',
+    'Oque é um txeto narrativo?',
+    'É um tipo de texto que conta uma história, real ou fictícia,'
+)
+criaCartao(
+    'Hisória',
+    'como ocorreu a gerra fria?',
+    'ocorreu após a Segunda Guerra Mundial, de 1945 a 1991, como um confronto ideológico entre os Estados Unidos (capitalista) e a União Soviética (socialista), que não envolveu um enfrentamento direto militar entre as duas superpotências. '
+)
+
+criaCartao(
+    'Projeto de Vida',
+    'oque você quer fazer quando crescer?',
+    'ser cozinheiro e professor de quimica'
+)
